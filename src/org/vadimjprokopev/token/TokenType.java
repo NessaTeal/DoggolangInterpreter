96db7b8a6b34aac5beb4b240bb5ed446ac419e6e
Vadim Prokopev <Ishvarloiva@gmail.com>
2019-05-19 20:36:27 +0300

    Create TokenType enum

---
+++ src/org/vadimjprokopev/token/TokenType.java
@@ -0,0 +1,20 @@
+package org.vadimjprokopev.token;
+
+public enum TokenType {
+    SET,
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    LESS,
+    MORE,
+    IF,
+    THEN_IF,
+    ELSE,
+    END_IF,
+    WHILE,
+    THEN_WHILE,
+    END_WHILE,
+    VARIABLE,
+    CONSTANT,
+    EOF
+}
