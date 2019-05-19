d4b576069578a50e1fff29cb864e507e25ca8329
Vadim Prokopev <Ishvarloiva@gmail.com>
2019-05-19 20:38:56 +0300

    Connect everything together

--- src/org/vadimjprokopev/DoggolangExecutor.java
+++ src/org/vadimjprokopev/DoggolangExecutor.java
@@ -1,5 +1,23 @@
 package org.vadimjprokopev;
+
+import org.vadimjprokopev.expression.ExpressionParser;
+import org.vadimjprokopev.token.Token;
+import org.vadimjprokopev.token.Tokenizer;
+
+import java.util.*;
+
 public class DoggolangExecutor {
+
     public static void main(String[] args) {
+        Map<String, Integer> variables = new HashMap<>();
+        Tokenizer tokenizer = new Tokenizer();
+        List<String> sourceCode = new ArrayList<>(Arrays.asList(args));
+        sourceCode.add("EOF");
+        List<Token> tokens = tokenizer.convertSourceCodeToTokens(sourceCode);
+        ExpressionParser expressionParser = new ExpressionParser(tokens);
+
+        while (expressionParser.hasNext()) {
+            expressionParser.getNextExpression().execute(variables);
+        }
     }
 }
